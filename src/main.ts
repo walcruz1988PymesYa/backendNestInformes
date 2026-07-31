690b27b3d7b35dc2c1b0a3b0f942160efc8d19c9
Walter Cruz <cruzherreria1@gmail.com>
2026-07-31 16:19:13 -0300

    mdoified main because fix cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/nest');
+    app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://www.sistemapy.com.ar',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
